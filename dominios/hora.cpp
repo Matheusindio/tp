c2b0d7892c33e8bebedb9be5fc44d94cd67bf235
Luis Gustavo <145784192+0luisg@users.noreply.github.com>
2024-11-26 17:59:02 -0300

    Update hora.cpp

--- dominios/hora.cpp
+++ dominios/hora.cpp
@@ -1,25 +1,15 @@
 #include "hora.hpp"
 
-bool Horario::validarHora(int hora) {
-    return hora >= 0 && hora <= 23;
+bool Horario::validarHora(int hora, int minuto) {
+    return hora >= 0 && hora <= 23 && minuto >= 0 && minuto <= 59;
 }
 
-bool Horario::validarMinuto(int minuto) {
-    return minuto >= 0 && minuto <= 59;
-}
-
-bool Horario::setHora(int hora) {
-    if (!validarHora(hora)) {
+bool Horario::setHora(int hora, int minuto) {
+    if (!validarHora(hora, minuto)) {
         return false;
     }
     this->hora = hora;
-    return true;
-}
-
-bool Horario::setMinuto(int minuto) {
-    if (!validarMinuto(minuto)) {
-        return false;
-    }
     this->minuto = minuto;
     return true;
 }
+
