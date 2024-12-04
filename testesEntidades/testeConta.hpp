4e03c7b393bebd1b2fff7144376f267f2cbefc0a
Luis Gustavo <145784192+0luisg@users.noreply.github.com>
2024-12-04 17:53:04 -0300

    Add files via upload
    falta fazer a main

---
+++ testesEntidades/testeConta.hpp
@@ -0,0 +1,21 @@
+#include "../entidades/conta.hpp"
+#include "../dominios/codigo.hpp"
+#include "../dominios/senha.hpp"
+
+class TUConta{
+    private:
+        const std::string VALOR_VALIDO_CODIGO = "012345";
+        const std::string VALOR_VALIDO_SENHA = "ABCD1";
+        Conta *conta;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenario();
+
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+}
+
+
