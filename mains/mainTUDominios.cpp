88394f0d11b74bdeb2eb288e864d155efdfa99aa
João Filipe de Lima <joaofilipeamdelima@gmail.com>
2024-12-05 08:58:21 -0300

    main att

--- mains/mainTUDominios.cpp
+++ mains/mainTUDominios.cpp
@@ -8,6 +8,8 @@
 #include "../testesDominios/testeHorario.cpp"
 #include "../testesDominios/testeNome.cpp"
 #include "../testesDominios/testeSenha.cpp"
+#include "../testesEntidades/testeviagem.cpp"
+#include "../testesEntidades/testeDestino.cpp"
 
 
 using namespace std;
@@ -17,90 +19,110 @@ int main()
     TUAvaliacao testeAvaliacao;
     switch(testeAvaliacao.run())
     {
-        case TUAvaliacao::SUCESSO:cout << "SUCESSO" << endl;
+        case TUAvaliacao::SUCESSO:cout << "SUCESSO Dominio Avaliacao" << endl;
         break;
 
 
-        case TUAvaliacao::FALHA:cout << "FALHA" << endl;
+        case TUAvaliacao::FALHA:cout << "FALHA Dominio Avaliacao" << endl;
         break;
     }
 
         TUCodigo testeCodigo;
     switch(testeCodigo.run())
     {
-        case TUCodigo::SUCESSO:cout << "SUCESSO" << endl;
+        case TUCodigo::SUCESSO:cout << "SUCESSO Dominio Codigio" << endl;
         break;
 
 
-        case TUCodigo::FALHA:cout << "FALHA" << endl;
+        case TUCodigo::FALHA:cout << "FALHA Dominio Codigo" << endl;
         break;
     }
 
         TUData testeData;
     switch(testeData.run())
     {
-        case TUData::SUCESSO:cout << "SUCESSO" << endl;
+        case TUData::SUCESSO:cout << "SUCESSO Dominio Data" << endl;
         break;
 
 
-        case TUData::FALHA:cout << "FALHA" << endl;
+        case TUData::FALHA:cout << "FALHA Dominio Data" << endl;
         break;
     }
 
         TUDinheiro testeDinheiro;
     switch(testeDinheiro.run())
     {
-        case TUDinheiro::SUCESSO:cout << "SUCESSO" << endl;
+        case TUDinheiro::SUCESSO:cout << "SUCESSO Dominio Dinheiro" << endl;
         break;
 
 
-        case TUDinheiro::FALHA:cout << "FALHA" << endl;
+        case TUDinheiro::FALHA:cout << "FALHA Dominio Dinheiro" << endl;
         break;
     }
 
         TUDuracao testeDuracao;
     switch(testeDuracao.run())
     {
-        case TUDuracao::SUCESSO:cout << "SUCESSO" << endl;
+        case TUDuracao::SUCESSO:cout << "SUCESSO Dominio Duracao" << endl;
         break;
 
 
-        case TUDuracao::FALHA:cout << "FALHA" << endl;
+        case TUDuracao::FALHA:cout << "FALHA Dominio Duracao" << endl;
         break;
     }
 
         TUHorario testeHorario;
     switch(testeHorario.run())
     {
-        case TUHorario::SUCESSO:cout << "SUCESSO" << endl;
+        case TUHorario::SUCESSO:cout << "SUCESSO Dominio Horario" << endl;
         break;
 
 
-        case TUHorario::FALHA:cout << "FALHA" << endl;
+        case TUHorario::FALHA:cout << "FALHA Dominio Horario" << endl;
         break;
     }
 
         TUNome testeNome;
     switch(testeNome.run())
     {
-        case TUNome::SUCESSO:cout << "SUCESSO" << endl;
+        case TUNome::SUCESSO:cout << "SUCESSO Dominio Nome" << endl;
         break;
 
 
-        case TUNome::FALHA:cout << "FALHA" << endl;
+        case TUNome::FALHA:cout << "FALHA Dominio Nome" << endl;
         break;
     }
 
         TUSenha testeSenha;
     switch(testeSenha.run())
     {
-        case TUSenha::SUCESSO:cout << "SUCESSO" << endl;
+        case TUSenha::SUCESSO:cout << "SUCESSO Dominio Senha" << endl;
         break;
 
 
-        case TUSenha::FALHA:cout << "FALHA" << endl;
+        case TUSenha::FALHA:cout << "FALHA Dominio Senha" << endl;
         break;
     }
 
+        TUViagem testeViagem;
+        switch(testeViagem.run())
+    {
+        case TUViagem::SUCESSO:cout << "SUCESSO Entidade Viagem" << endl;
+        break;
+
+
+        case TUViagem::FALHA:cout << "FALHA Entidade Viagem" << endl;
+        break;
+    }
+    
+        TUDestino testeDestino;
+        switch (testeDestino.run())
+        {
+        case TUDestino::SUCESSO:cout << "SUCESSO Entidade Destino" << endl;
+            break;
+        
+        case TUDestino::FALHA:cout << "FALHA Entidade Destino" << endl;
+            break;
+        }
     return 0;
 }
